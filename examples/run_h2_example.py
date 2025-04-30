d92c68b2a5dca9a22f9be8687bb30bdd4df7f416
7Abdoman7 <abdo.elsayd102@gmail.com>
2025-04-30 23:04:37 +0300

    added a 2 qubit example

--- examples/run_h2_example.py
+++ examples/run_h2_example.py
@@ -2,15 +2,14 @@ import numpy as np
 from easy_vqe import find_ground_state, draw_final_bound_circuit, print_results_summary, get_theoretical_ground_state_energy
 
 # --- Define Hamiltonian ---
-# Example: Simpler 3-Qubit Hamiltonian (adjust as needed)
-hamiltonian_3q = "-ZZI+0.9*ZIZ-0.5*IZZ+0.2*XXX-0.3*YYY"
+# Example: Simpler 2-Qubit Hamiltonian (adjust as needed)
+hamiltonian_2q = "2*II-2*XX+3*YY-3*ZZ"
 
 # --- Define Ansatz Structure ---
 ansatz_block_linear_ent = [
-    ('ry', [0, 1, 2]),
+    ('ry', [0, 1]),
     ('cx', [0, 1]),
-    ('cx', [1, 2]), 
-    ('rz', [0, 1, 2]),
+    ('rz', [0, 1]),
 ]
 
 ansatz_structure = [
@@ -25,13 +24,13 @@ print("Starting VQE calculation...")
 
 results = find_ground_state(
     ansatz_structure=ansatz_structure,
-    hamiltonian_expression=hamiltonian_3q,
+    hamiltonian_expression=hamiltonian_2q,
     n_shots=8192,
     optimizer_method='COBYLA',
     optimizer_options={'maxiter': 500, 'rhobeg': 0.5, 'tol': 1e-5}, 
     initial_params_strategy='random',
     display_progress=True,
-    plot_filename="h3_convergence.png" 
+    plot_filename="h2_convergence.png" 
 )
 
 # --- Print Summary from Results Dictionary ---
@@ -41,7 +40,7 @@ print_results_summary(results)
 draw_final_bound_circuit(results)
 
 # --- Theoretical Ground State Energy ---
-theoretical_energy = get_theoretical_ground_state_energy(hamiltonian_3q)
+theoretical_energy = get_theoretical_ground_state_energy(hamiltonian_2q)
 print(f"Theoretical Ground State Energy: {theoretical_energy}")
 
 # --- Compare with VQE Result ---
